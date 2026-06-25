9eb672bdea15149a4c6396cbc1963d4dcb31a33c
pratimamaiti13 <pratima.maiti76@gmail.com>
2026-06-25 11:18:19 +0530

    Enabled ECR scan on push to scan vulnerabilities before pushing into ECR

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -10,6 +10,10 @@ resource "aws_ecr_repository" "this" {
   name                 = "${var.project_name}-${each.value}"
   image_tag_mutability = "MUTABLE"
 
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
   encryption_configuration {
     encryption_type = "AES256"
   }
